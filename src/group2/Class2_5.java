e229a488e7b321016cf74b70d5906f6f151b2e3d
JuryDadian <filejob@mail.ru>
2022-11-26 18:41:07 +0300

    Урок  2_5

---
+++ src/group2/Class2_5.java
@@ -0,0 +1,11 @@
+package group2;
+
+public class Class2_5 {
+    public static void main(String[] args) {
+        int a = 0;
+        for (int i = 0; i < 10; i++) {
+        System.out.println(a);
+        a = a - 5;
+        }
+    }
+}
