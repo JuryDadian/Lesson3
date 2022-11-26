ab1ca598583fac50e3a88d3973261d699c7fb7f6
JuryDadian <filejob@mail.ru>
2022-11-26 15:19:02 +0300

    Урок  2_1

---
+++ src/group2/Class2_1.java
@@ -0,0 +1,13 @@
+package group2;
+
+import java.util.Scanner;
+
+public class Class2_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100 ; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
