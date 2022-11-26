3c132631d925500c87d2388857110cd519bd88ad
JuryDadian <filejob@mail.ru>
2022-11-26 17:00:10 +0300

    Урок  2_4

---
+++ src/group2/Class2_4.java
@@ -0,0 +1,11 @@
+package group2;
+
+public class Class2_4 {
+    public static void main(String[] args) {
+        int number = 7;
+        while (number <= 98) {
+            System.out.print(number + " ");
+            number = number + 7;
+        }
+    }
+}
