7bca3be867095598df5e8a5b60de65639b91b4a7
JuryDadian <filejob@mail.ru>
2022-11-26 13:38:30 +0300

    Урок  1

--- src/group1/Class1_1.java
+++ src/group1/Class1_1.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Class1_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите требуемый месяц в году");
-        String month = scanner.nextLine();
+        System.out.println("Введите номер месяца");
+        int month = scanner.nextInt();
         switch (month) {
-            case "декабрь", "январь", "февраль" :
+            case 12, 1, 2 :
                 System.out.println("Зима");
                 break;
-            case "март", "апрель", "май" :
+            case 3, 4, 5 :
                 System.out.println("Весна");
                 break;
-            case "июнь", "июль", "август" :
+            case 6, 7, 8 :
                 System.out.println("Лето");
                 break;
-            case "сентябрь", "октябрь", "ноябрь" :
+            case 9, 10, 11 :
                 System.out.println("Осень");
                 break;
         }
