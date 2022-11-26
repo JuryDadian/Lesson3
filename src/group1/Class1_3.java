9a29d22d4159a33863c1c69ba4f4e39f287262b8
JuryDadian <filejob@mail.ru>
2022-11-26 14:09:06 +0300

    Урок  3

---
+++ src/group1/Class1_3.java
@@ -0,0 +1,16 @@
+package group1;
+
+import java.util.Scanner;
+
+public class Class1_3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Четное число");
+        } else {
+            System.out.println("Нечетное число");
+        }
+    }
+}
