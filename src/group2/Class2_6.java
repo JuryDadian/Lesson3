128b5764886ea50eb90fb6130a70a29c8fa5c348
JuryDadian <filejob@mail.ru>
2022-11-26 18:47:02 +0300

    Урок  2_6

---
+++ src/group2/Class2_6.java
@@ -0,0 +1,9 @@
+package group2;
+
+public class Class2_6 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20 ; i++) {
+            System.out.println(i*i);
+        }
+    }
+}
